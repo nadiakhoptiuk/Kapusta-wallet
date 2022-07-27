ad7d8e59b5e6d64cf9fbd0af1f5731e04269d781
Vladyslav Avidzba <wasssd1@gmail.com>
2022-07-27 20:32:21 +0300

    path-fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,7 +15,7 @@ export const App = () => {
         <Route path={home} element={<SharedLayout />}>
           <Route index element={<div>Auth (Home) View</div>} />
           <Route path={app} element={<div>Balance Bar View</div>}>
-            <Route index element={<div>Transactions View</div>}>
+            <Route path={app} element={<div>Transactions View</div>}>
               <Route path={expenses} element={<div>Expenses Table</div>} />
               <Route path={income} element={<div>Income Table</div>} />
             </Route>
