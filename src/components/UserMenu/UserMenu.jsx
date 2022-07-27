f1c33d031fea140f13fcf9db55a4cc514fbc4cd6
Iuliia <iuliiaiavorskaia@gmail.com>
2022-07-27 10:18:29 +0300

    fix heder

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -2,31 +2,51 @@
 // import authSelectors from 'redux/auth/auth-selectors';
 // import authOperations from 'redux/auth/auth-operations';
 import { Avatar } from '@mui/material';
+import React, { Fragment } from 'react';
+import Media from 'react-media';
 import stringAvatar from '../../utils/Avatar';
 import s from './UserMenu.module.css';
 
 const UserMenu = () => {
   //   const userEmail = useSelector(authSelectors.userEmail);
   //   const dispatch = useDispatch();
-  const name = 'iuliia@gmail.com';
+  const name = 'alina@gmail.com';
   return (
     <div className={s.menu}>
       <Avatar {...stringAvatar(name.toUpperCase())} />
-
-      <span className={s.email}>iuliia@gmail.com</span>
-
-      <button
-        className={s.logout}
-        // onClick={() => dispatch(authOperations.logout())}
+      <Media
+        queries={{
+          small: '(max-width: 767px)',
+          medium: '(min-width: 768px)',
+          // large: '(min-width: 1280px)',
+        }}
       >
-        <img
-          src={require('../../images/logout.jpg')}
-          alt="logout"
-          width={16}
-          height={16}
-        />
-      </button>
-      <button className={s.btnExit}>Exit</button>
+        {matches => (
+          <Fragment>
+            {matches.small && (
+              <button
+                className={s.logout}
+                // onClick={() => dispatch(authOperations.logout())}
+              >
+                <img
+                  src={require('../../images/logout.jpg')}
+                  alt="logout"
+                  width={16}
+                  height={16}
+                />
+              </button>
+            )}
+            {matches.medium && (
+              <>
+                <span className={s.email}>iuliia@gmail.com</span>
+                <span className={s.line}></span>
+                <button className={s.btnExit}>Exit</button>
+              </>
+            )}
+            {/* {matches.large && <p>I am large!</p>} */}
+          </Fragment>
+        )}
+      </Media>
     </div>
   );
 };
