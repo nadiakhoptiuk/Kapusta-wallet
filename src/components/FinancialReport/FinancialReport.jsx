a091b13d6aac38361f67831d2b7cd76d575c5d19
Nadia Khoptiuk <holineinadia@gmail.com>
2022-07-27 17:08:22 +0300

    add mobile, tablet and desktop ver

---
+++ src/components/FinancialReport/FinancialReport.jsx
@@ -0,0 +1,82 @@
+import Sprite from '../../images/sprite.svg';
+import { useState } from 'react';
+import s from './FinancialReport.module.css';
+import { expensesCategory, incomeCategory } from 'utils/localization';
+
+export default function FinancialReport() {
+  const [selectedCategory, setSelectedCategory] = useState(
+    expensesCategory[0].category
+  );
+  const [selectedOperation, setSelectedOperation] = useState('expenses');
+  console.log(selectedCategory);
+
+  function transformedString(string) {
+    if (!string.includes(' ')) {
+      return string;
+    }
+
+    return string.split(' ').map((str, index) => {
+      console.log(str);
+      return <span key={index}>{str}</span>;
+    });
+  }
+
+  function addSpaceForAmount() {}
+
+  function handleCategoryClick(newCategoryName) {
+    setSelectedCategory(newCategoryName);
+  }
+
+  return (
+    <section className={s.reportSection}>
+      <div className={s.controls}>
+        <button type="button" className={s.typeReportControlBtn}>
+          <svg className={s.arrowIcon}>
+            <use href={`${Sprite}#arrow-prev-icon`}></use>
+          </svg>
+        </button>
+        <h2 className={s.reportTypeTitle}>Expenses</h2>
+        <button type="button" className={s.typeReportControlBtn}>
+          <svg className={s.arrowIcon}>
+            <use href={`${Sprite}#arrow-next-icon`}></use>
+          </svg>
+        </button>
+      </div>
+
+      <ul className={s.categoryList}>
+        {expensesCategory.map(({ total, imgPath, category }, index) => {
+          return total === 0 ? null : (
+            <li key={index} className={s.categoryItem}>
+              <span className={s.categoryAmount}>{total.toFixed(2)}</span>
+
+              <div className={s.btnWrap}>
+                <button
+                  type="button"
+                  className={
+                    category === selectedCategory
+                      ? `${s.categoryBtnActive}`
+                      : `${s.categoryBtn}`
+                  }
+                  onClick={() => handleCategoryClick(category)}
+                >
+                  <svg className={s.categoryIcon}>
+                    <use href={`${Sprite}#${imgPath}`}></use>
+                  </svg>
+                </button>
+                <div
+                  className={
+                    category === selectedCategory
+                      ? `${s.rectangleActive}`
+                      : `${s.rectangle}`
+                  }
+                ></div>
+              </div>
+
+              <p className={s.category}>{transformedString(category)}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+}
---
+++ src/utils/transactionTypes.js
@@ -0,0 +1,6 @@
+const transactionTypes = [
+  { category: 'Expenses', queryString: 'expense' },
+  { category: 'Incomes', queryString: 'income' },
+];
+
+export default transactionTypes;
