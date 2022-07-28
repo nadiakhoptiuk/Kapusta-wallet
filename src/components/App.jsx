cec87f8c2cddc60f68ce4e3e145bb180e4e8f8c2
Alexander Pustosmekhov <a.pustosmekhov@gmail.com>
2022-07-28 15:11:46 +0300

    fix bugs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,25 +7,30 @@ import 'react-toastify/dist/ReactToastify.css';
 import routes from 'utils/routes';
 import HomeView from 'views/HomeView';
 import ReportView from 'views/ReportView';
+import Balance from './Balance';
+import Container from './Container/Container';
 
 const { home, app, reports, income, expenses } = routes;
 
 export const App = () => {
   return (
-    <Fragment>
-      <Routes>
-        <Route path={home} element={<SharedLayout />}>
-          <Route index element={<HomeView />} />
-          <Route path={app} element={<div>Balance Bar View</div>}>
-            <Route path={app} element={<div>Transactions View</div>}>
-              <Route path={expenses} element={<div>Expenses Table</div>} />
-              <Route path={income} element={<div>Income Table</div>} />
-            </Route>
-            <Route path={reports} element={<ReportView />} />
-          </Route>
-        </Route>
-      </Routes>
-      <ToastContainer />
-    </Fragment>
+    // <Fragment>
+    //   <Routes>
+    //     <Route path={home} element={<SharedLayout />}>
+    //       <Route index element={<HomeView />} />
+    //       <Route path={app} element={<div>Balance Bar View</div>}>
+    //         <Route path={app} element={<div>Transactions View</div>}>
+    //           <Route path={expenses} element={<div>Expenses Table</div>} />
+    //           <Route path={income} element={<div>Income Table</div>} />
+    //         </Route>
+    //         <Route path={reports} element={<ReportView />} />
+    //       </Route>
+    //     </Route>
+    //   </Routes>
+    //   <ToastContainer />
+    // </Fragment>
+    <Container>
+      <Balance />
+    </Container>
   );
 };
--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,21 +1,35 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import s from './Balance.module.css';
 import sprite from '../../images/sprite.svg';
-import BalanceModal from 'components/BalanceModal';
+// import BalanceModal from 'components/BalanceModal';
+import routes from 'utils/routes';
 
-const isReportPage = true;
+const { app, reports } = routes;
+
+const isReportPage = false;
 
 const balanceRow = s.Balance;
 const balanceRowRevers = s.BalanceRevers;
 const classBalance = isReportPage ? balanceRowRevers : balanceRow;
 
 export default function Balance() {
+  const [balance, setBalance] = useState(0);
+
+  const handleChange = e => {
+    setBalance(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    // console.log(e);
+  };
+
   return (
     <Fragment>
       <div className={classBalance}>
         {isReportPage && (
-          <Link to="/" className={s.linkToHome}>
+          <Link to={app} className={s.linkToHome}>
             <svg width="18" height="12">
               <use href={`${sprite}#arrow-to-main-icon`}></use>
             </svg>
@@ -42,7 +56,7 @@ export default function Balance() {
               </div>
             </div>
           ) : (
-            <Link to="/" className={s.linkReports}>
+            <Link to={reports} className={s.linkReports}>
               Reports
               <svg width="14" height="14" className={s.reportsIcon}>
                 <use href={`${sprite}#report-icon`}></use>
@@ -51,14 +65,17 @@ export default function Balance() {
           )}
 
           {!isReportPage ? (
-            <form className={s.form}>
+            <form className={s.form} onSubmit={handleSubmit}>
               <label className={s.label}>
                 <span className={s.labelText}>Balance:</span>
 
                 <input
                   className={s.input}
                   type="number"
-                  placeholder="00.00 UAH"
+                  // placeholder="00.00 UAH"
+                  name="balance"
+                  value={balance}
+                  onChange={handleChange}
                 />
                 {!isReportPage && (
                   <button type="submit" className={s.buttonForm}>
