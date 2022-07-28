fb333314f04a242e3756ea45b09c6deeff4c13d6
Vlad-Kozak <k911k12@gmail.com>
2022-07-28 10:22:29 +0300

    done errors and validation form

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -7,14 +7,21 @@ export default function AuthForm() {
   const dispatch = useDispatch();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [emptyInput, setEmptyInput] = useState(false);
+  const [invalidEmail, setInvalidEmail] = useState(false);
+  const [smallLengthPassword, setSmallLengthPassword] = useState(false);
 
   const onChange = e => {
+    setEmptyInput(false);
+
     switch (e.currentTarget.name) {
       case 'email':
+        setInvalidEmail(false);
         setEmail(e.currentTarget.value);
         break;
 
       case 'password':
+        setSmallLengthPassword(false);
         setPassword(e.currentTarget.value);
         break;
 
@@ -23,18 +30,52 @@ export default function AuthForm() {
     }
   };
 
-  const handleLogin = () => {
+  const handleLogin = e => {
+    e.preventDefault();
+
+    if (checkForEmptyInput() || checkValidEmail() || checkLengthPassword()) {
+      return;
+    }
+
     dispatch(authOperations.login({ email, password }));
     formReset();
   };
 
   const handleRegister = () => {
+    if (checkForEmptyInput() || checkValidEmail() || checkLengthPassword()) {
+      return;
+    }
+
     dispatch(authOperations.register({ email, password }));
     // formReset();
   };
 
   const handleGoogleLogin = () => {
     // dispatch(authOperations.googleLogin());
+  };
+
+  const checkForEmptyInput = () => {
+    if (email === '' || password === '') {
+      setEmptyInput(true);
+      return true;
+    }
+    return false;
+  };
+
+  const checkValidEmail = () => {
+    if (!email.includes('@')) {
+      setInvalidEmail(true);
+      return true;
+    }
+    return false;
+  };
+
+  const checkLengthPassword = () => {
+    if (password.length < 8) {
+      setSmallLengthPassword(true);
+      return true;
+    }
+    return false;
   };
 
   const formReset = () => {
@@ -57,44 +98,71 @@ export default function AuthForm() {
       <p className={s.authText}>
         Or log in using an email and password, after registering:
       </p>
-      <label className={s.label}>
-        Email:
-        <input
-          className={s.input}
-          autoComplete="off"
-          type="email"
-          placeholder="your@email.com"
-          value={email}
-          name="email"
-          required
-          onChange={onChange}
-        />
-      </label>
-      <label className={s.label}>
-        Password:
-        <input
-          className={s.input}
-          autoComplete="off"
-          type="password"
-          placeholder="Password"
-          value={password}
-          name="password"
-          required
-          onChange={onChange}
-        />
-      </label>
-      <div className={s.authBtnBox}>
-        <button onClick={handleLogin} className={s.loginBtn} type="button">
-          Log in
-        </button>
-        <button
-          onClick={handleRegister}
-          className={s.registerBtn}
-          type="button"
-        >
-          Registration
-        </button>
-      </div>
+      <form onSubmit={handleLogin}>
+        <label className={s.label}>
+          {emptyInput ? (
+            <>
+              <span>
+                <span className={s.labelStar}>*</span>
+                Email:
+              </span>
+            </>
+          ) : (
+            'Email:'
+          )}
+          <input
+            className={s.input}
+            autoComplete="off"
+            type="text"
+            placeholder="your@email.com"
+            value={email}
+            name="email"
+            onChange={onChange}
+          />
+          <p className={s.errorMsg}>
+            {invalidEmail && 'Email must contain the symbol "@"'}
+            {emptyInput && 'This is a required field'}
+          </p>
+        </label>
+        <label className={s.label}>
+          {emptyInput ? (
+            <>
+              <span>
+                <span className={s.labelStar}>*</span>
+                Password:
+              </span>
+            </>
+          ) : (
+            'Password:'
+          )}
+          <input
+            className={s.input}
+            autoComplete="off"
+            type="password"
+            placeholder="Password"
+            value={password}
+            name="password"
+            onChange={onChange}
+          />
+          <p className={s.errorMsg}>
+            {smallLengthPassword &&
+              'Password length must be at least 8 characters'}
+            {emptyInput && 'This is a required field'}
+          </p>
+        </label>
+        <div className={s.authBtnBox}>
+          <button className={s.loginBtn} type="submit">
+            Log in
+          </button>
+          <button
+            onClick={handleRegister}
+            className={s.registerBtn}
+            type="button"
+          >
+            Registration
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
--- src/redux/auth/auth-slice.js
+++ src/redux/auth/auth-slice.js
@@ -1,3 +1,4 @@
+import { toast } from 'react-toastify';
 import { createSlice } from '@reduxjs/toolkit';
 import { authOperations } from './auth-operations';
 
@@ -7,16 +8,46 @@ const initialState = {
   refreshToken: null,
   sid: null,
   isLoggedIn: false,
-  // isLoadingRefresh: false,
+  isLoadingRegister: false,
+  isLoadingLogin: false,
+  isLoadingLogout: false,
+  isLoadingRefresh: false,
 };
 
 const authSlice = createSlice({
   name: 'auth',
   initialState,
   extraReducers: {
+    // register
+    [authOperations.register.pending](state) {
+      state.isLoadingRegister = true;
+    },
     [authOperations.register.fulfilled](state, action) {
       state.userData.email = action.payload.email;
       state.userData.id = action.payload.id;
+      state.isLoadingRegister = false;
+      toast('You have successfully registered');
+      toast('Now you can login');
+    },
+    [authOperations.register.rejected](state, action) {
+      state.isLoadingRegister = false;
+      switch (action.payload) {
+        case 400:
+          toast.error('Wrong email or password, please try again.');
+          break;
+
+        case 409:
+          toast.error('This email is already being used');
+          break;
+
+        default:
+          toast.error('Server error, please try again later');
+      }
+    },
+
+    // login
+    [authOperations.login.pending](state) {
+      state.isLoadingLogin = true;
     },
     [authOperations.login.fulfilled](state, action) {
       state.userData = action.payload.userData;
@@ -24,6 +55,27 @@ const authSlice = createSlice({
       state.refreshToken = action.payload.refreshToken;
       state.sid = action.payload.sid;
       state.isLoggedIn = true;
+      state.isLoadingLogin = false;
+    },
+    [authOperations.login.rejected](state, action) {
+      state.isLoadingLogin = false;
+      switch (action.payload) {
+        case 400:
+          toast.error('Wrong email or password, please try again.');
+          break;
+
+        case 403:
+          toast.error('Wrong email or password, please try again.');
+          break;
+
+        default:
+          toast.error('Server error, please try again later');
+      }
+    },
+
+    // logout
+    [authOperations.logout.pending](state) {
+      state.isLoadingLogout = true;
     },
     [authOperations.logout.fulfilled](state) {
       state.userData = { email: null, id: null, balance: 0, transactions: [] };
@@ -31,16 +83,44 @@ const authSlice = createSlice({
       state.refreshToken = null;
       state.sid = null;
       state.isLoggedIn = false;
+      state.isLoadingLogout = false;
+    },
+    [authOperations.logout.rejected](state) {
+      state.isLoadingLogout = false;
+      toast.error('Server error, please try again later');
+    },
+
+    // refresh
+    [authOperations.fetchCurrentUser.pending](state) {
+      state.isLoadingRefresh = true;
     },
     [authOperations.fetchCurrentUser.fulfilled](state, action) {
       state.accessToken = action.payload.newAccessToken;
       state.refreshToken = action.payload.newRefreshToken;
       state.sid = action.payload.data.newSid;
     },
-    [authOperations.googleLogin.fulfilled](state, action) {},
+    [authOperations.fetchCurrentUser.rejected](state) {
+      state.isLoadingRefresh = false;
+    },
     [authOperations.getUserData.fulfilled](state, action) {
       state.userData = action.payload;
       state.isLoggedIn = true;
+      state.isLoadingRefresh = false;
+    },
+    [authOperations.getUserData.rejected](state) {
+      state.isLoadingRefresh = false;
+    },
+
+    // google
+    [authOperations.googleLogin.pending](state) {
+      state.isLoadingLogin = true;
+    },
+    [authOperations.googleLogin.fulfilled](state, action) {
+      state.isLoadingLogin = false;
+    },
+    [authOperations.googleLogin.rejected](state) {
+      state.isLoadingLogin = false;
+      toast.error('error');
     },
   },
 });
