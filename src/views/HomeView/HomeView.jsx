9a1a198048567c3ee0e055c80ad23687b46d3ac8
Maksym Shumeiko <kkiilleerraa@mail.ru>
2022-07-28 16:47:30 +0300

    update-comit

--- src/views/HomeView/HomeView.jsx
+++ src/views/HomeView/HomeView.jsx
@@ -1,11 +1,13 @@
 import sprite from '../../images/sprite.svg';
 import AuthForm from 'components/AuthForm/';
 import s from './HomeView.module.css';
+import Animation from 'components/Animation';
 
 const HomeView = () => {
   return (
     <main className={s.view}>
       <div className={s.top}>
+        <Animation />
         <div className={s.topWrapper}>
           <svg className={s.title}>
             <use href={`${sprite}#icon-kapusta`}></use>
