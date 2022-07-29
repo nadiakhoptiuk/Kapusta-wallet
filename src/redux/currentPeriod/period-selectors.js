90a53feb2b54ffe24c86f8a7aaff9dbebd710bcb
Alexander Pustosmekhov <a.pustosmekhov@gmail.com>
2022-07-29 15:45:46 +0300

    add selector

--- src/redux/currentPeriod/period-selectors.js
+++ src/redux/currentPeriod/period-selectors.js
@@ -2,3 +2,6 @@ export const currentPeriodDataSelector = state =>
   state.currentPeriodData.periodData;
 
 export const isLoadingSelector = state => state.currentPeriodData.isLoading;
+
+export const currentPeriodSelector = state =>
+  state.currentPeriodData.currentPeriod;
