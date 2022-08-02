c18c043456cf69a4cf7d6bdeb166b98889c906d7
Nadia Khoptiuk <holineinadia@gmail.com>
2022-08-02 22:28:04 +0300

    fix something, move some styles reset and some delete

--- src/utils/localization.js
+++ src/utils/localization.js
@@ -3,67 +3,56 @@ export const expensesCategory = [
     backendName: 'Продукты',
     category: 'Products',
     imgPath: 'products-icon',
-    total: 0,
   },
   {
     backendName: 'Алкоголь',
     category: 'Alcohol',
     imgPath: 'alcohol-icon',
-    total: 0,
   },
   {
     backendName: 'Развлечения',
     category: 'Entertainment',
     imgPath: 'entertainment-icon',
-    total: 0,
   },
   {
     backendName: 'Здоровье',
     category: 'Health',
     imgPath: 'health-icon',
-    total: 0,
   },
   {
     backendName: 'Транспорт',
     category: 'Transport',
     imgPath: 'transport-icon',
-    total: 0,
   },
   {
     backendName: 'Всё для дома',
     category: 'Housing',
     imgPath: 'housing-icon',
-    total: 0,
   },
   {
     backendName: 'Техника',
     category: 'Technique',
     imgPath: 'technique-icon',
-    total: 0,
   },
   {
     backendName: 'Коммуналка и связь',
     category: 'Communal,\n communication',
     imgPath: 'communal-icon',
-    total: 0,
   },
   {
     backendName: 'Спорт и хобби',
     category: 'Sports,\n hobbies',
     imgPath: 'sports-icon',
-    total: 0,
   },
   {
     backendName: 'Образование',
     category: 'Education',
     imgPath: 'education-icon',
-    total: 0,
   },
   {
     backendName: 'Прочее',
     category: 'Other',
     imgPath: 'other-icon',
-    total: 0,
   },
 ];
 
@@ -72,12 +61,10 @@ export const incomeCategory = [
     backendName: 'З/П',
     category: 'Salary',
     imgPath: 'salary-icon',
-    total: 0,
   },
   {
     backendName: 'Доп. доход',
     category: 'Additional income',
     imgPath: 'add-income-icon',
-    total: 0,
   },
 ];
