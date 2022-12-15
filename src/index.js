7e54de7e7dff5901455835bb5ff77d727e830a0c
Nadiia Khoptiuk <holineinadia@gmail.com>
2022-12-15 18:13:52 +0200

    change basename and homepage

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/react-kapusta-project/">
+        <BrowserRouter basename="/Kapusta-wallet/">
           <App />
         </BrowserRouter>
       </PersistGate>
